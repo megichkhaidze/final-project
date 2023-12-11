a902f148591c2540a7216884ecca76ae849c0b00
megichkhaidze <148057503+megichkhaidze@users.noreply.github.com>
2023-12-11 16:14:22 +0400

    scrolls added

--- js/jquery.js
+++ js/jquery.js
@@ -1,15 +1,23 @@
-var fixmeTop = $('.fixme').offset().top;
-$(window).scroll(function () {
-    var currentScroll = $(window).scrollTop();
-    if (currentScroll >= fixmeTop) {
-        $('.fixme').css({
-            position: 'fixed',
-            top: '0',
-            left: '0'
-        });
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000 ) {
+        mybutton.style.display = "block";
     } else {
-        $('.fixme').css({
-            position: 'static'
-        });
+        mybutton.style.display = "none";
     }
-});
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function scrollWin() {
+    window.scrollBy(0, 800);
+}
